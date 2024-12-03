1a1c6b70b6b0905d2b5004cf909c90485f996a7e
Neel Shah <neelasheshbhai.shah@sjsu.edu>
2024-12-02 20:42:44 -0800

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter,Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'], // Adjust as needed, e.g., 'latin', 'latin-ext'
+    weight: ['400', '700'], // Select weights to include
+  });
+
